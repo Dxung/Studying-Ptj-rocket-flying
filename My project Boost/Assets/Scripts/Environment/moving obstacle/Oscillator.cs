b0e5ffb23b0665be585c10614621f7eecb1ebacb
Dxung <dung.lx183505@gmail.com>
2022-06-09 17:49:36 +0700

    Finish
    - Add moving Obstacles
    - Add Lights
    --> Build game

---
+++ My project Boost/Assets/Scripts/Environment/moving obstacle/Oscillator.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Oscillator : MonoBehaviour
+{
+    [SerializeField] Vector3 movementVector;
+    [SerializeField] [Range(0,1)] float movementFactor;
+    [SerializeField] float period = 3f;
+    Vector3 startingPosition;
+    
+
+
+    private void Start()
+    {
+        startingPosition = transform.position;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(period <= Mathf.Epsilon) { return; }
+       
+        float cycles = Time.time / period;
+        const float val = Mathf.PI * 2;
+        float rawSinWave = Mathf.Sin(cycles * val);
+
+        movementFactor = (rawSinWave + 1f) / 2f;
+
+        Vector3 offset = movementVector * movementFactor;
+        transform.position = startingPosition + offset;
+
+    }
+}
